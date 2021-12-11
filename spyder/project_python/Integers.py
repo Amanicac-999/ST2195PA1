b36d47a28e4866b8cef93a27bc78dba00dd559c7
Aman_Tiwari <aman1999tiwari1@gmail.com>
2021-12-11 17:23:36 +0800

    programms to print 1 to 10

---
+++ spyder/project_python/Integers.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 11 12:51:43 2021
+
+@author: aman1
+"""
+
+#%%
+print(1,2,3,4,5,6,7,8,9,10)
+
+#%%
+for i in range(1,11):
+    print(i)
+print("DONE")
